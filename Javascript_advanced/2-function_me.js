f48e609b7f902cc4547b177220f080f510f29871
ayaaribi123 <ayaaribi11@gmail.com>
2023-05-14 21:52:03 +0100

    js file

--- Javascript_advanced/2-function_me.js
+++ Javascript_advanced/2-function_me.js
@@ -1,8 +1,8 @@
 function welcomeMessage (fullName){
 	return function (){
-		alert (Welcome + fullName)
+		alert ("Welcome" + fullName)
 	}
 }
-var guillaume = welcomeMessage(Guillaume);
-var alex = welcomeMessage(Alex);
-var fred = welcomeMessage(Fred);
+var guillaume = welcomeMessage("Guillaume");
+var alex = welcomeMessage("Alex");
+var fred = welcomeMessage("Fred");
